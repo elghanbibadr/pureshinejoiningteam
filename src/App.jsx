3e48d9e4ea76a56b9a49e0cbd7a6a1556f23ce8c
elghanbibadr <bghanbi50@gmail.com>
2024-05-05 15:01:45 +0200

    change loog fill

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ContactForm from './ContactForm'
 import BouncingBall from './BouncingBall'
-import logo from "../src/assets/logo.png"
+import logo from "../src/assets/logo6.png"
 const App = () => {
   return (
     <>
     <div >
-      <img className='h-32  sm:h-40 -left-10 md:left-0  object-contain absolute -top-14' src={logo} />
+      <img className='h-56  sm:h-72 w-72 -left-20 md:-left-20  object-contain absolute -top-14' src={logo} />
       <BouncingBall />
     </div>
     <ContactForm />
